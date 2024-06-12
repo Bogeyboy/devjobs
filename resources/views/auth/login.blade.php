8436bee8ea1b7cb80bcd6fa4115c9036ffd2c4b2
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-06-12 20:06:40 +0200

    Clase 178

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,8 +27,13 @@
         <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700
+                            text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
+                        name="remember">
+
+                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">
+                    {{ __('Recordarme') }}
+                </span>
             </label>
         </div>
 
@@ -48,8 +53,8 @@
 
             
         </div>
-        <x-primary-button class="ms-3">
-            {{ __('Log in') }}
+        <x-primary-button class="w-full justify-center">
+            {{ __('Log In') }}
         </x-primary-button>
     </form>
 </x-guest-layout>
