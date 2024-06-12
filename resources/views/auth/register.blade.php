55cefa9ae97759987493454a725c3b3bc3caa430
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-06-12 20:31:27 +0200

    Clase 179

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+    <form method="POST" action="{{ route('register') }}" novalidate>
         @csrf
 
         <!-- Name -->
@@ -39,14 +39,18 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
+        <div class="flex justify-between my-5">
+            <x-link :href="route('login')">
+                {{ __('Log In') }}
+            </x-link>
+        
+            <x-link :href="route('password.request')">
+                ¿Olvidaste tu contraseña?
+            </x-link>
         </div>
+
+        <x-primary-button class="w-full justify-center">
+            {{ __('Create Account') }}
+        </x-primary-button>
     </form>
 </x-guest-layout>
