e0fef9971b528a9294a4cd381f21b857ae86c2a6
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-06-15 08:45:38 +0200

    Clase 186

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\VacanteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+/* Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard'); */
+
+Route::get('/dashboard',[VacanteController::class,'index'])
+        ->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
