16379223a679b8edb370af2e7847185907bdd54d
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-06-17 17:45:21 +0200

    Clase 190

--- resources/views/livewire/crear-vacante.blade.php
+++ resources/views/livewire/crear-vacante.blade.php
@@ -1,3 +1,83 @@
-<div>
-    <h1>Creando la vacante desde livewire</h1>
-</div>
+<form action="" class="md:w1/2 space-y-5">
+    {{-- Titulo de la vacante --}}
+    <div>
+        <x-input-label for="titulo" :value="__('Titulo de la vacante')" />
+        <x-text-input
+            id="titulo"
+            class="block mt-1 w-full"
+            type="text"
+            name="titulo"
+            :value="old('titulo')"
+            placeholder="Titulo de la vacante"/>
+    </div>
+
+    {{-- Salario mensual --}}
+    <div>
+        <x-input-label for="salario" :value="__('Salario mensual')" />
+        <x-select id="salario" name="salario" type="select" required>
+            <option value="">-- Selecciona un tipo de cuenta --</option>
+        </x-select>
+    </div>
+
+    {{-- Categoria --}}
+    <div>
+        <x-input-label for="categoria" :value="__('Categoria')" />
+        <x-select id="salario" name="salario" type="select" required>
+            <option value="">-- Selecciona un tipo de cuenta --</option>
+        </x-select>
+    </div>
+
+    {{-- Empresa --}}
+    <div>
+        <x-input-label for="empresa" :value="__('Empresa')" />
+        <x-text-input
+            id="empresa"
+            class="block mt-1 w-full"
+            type="text"
+            name="empresa"
+            :value="old('empresa')"
+            placeholder="Nombre de la empresa" />
+        <x-input-error :messages="$errors->get('empresa')" class="mt-2" />
+    </div>
+
+    {{-- Último día para presentar candidaturas --}}
+    <div>
+        <x-input-label for="ultimo_dia" :value="__('Último día para las candidaturas')" />
+        <x-text-input
+            id="ultimo_dia"
+            class="block mt-1 w-full"
+            type="date"
+            name="ultimo_dia"
+            :value="old('empresa')" />
+    </div>
+
+    {{-- Descripción del puesto de trabajo --}}
+    <div>
+        <x-input-label for="descripcion" :value="__('Descripción del puesto')" />
+        <textarea
+            id="descripcion"
+            class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500
+                        dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm w-full h-75"
+            name="descripcion"
+            placeholder="Descripción general del puesto de trabajo">
+        </textarea>
+    </div>
+
+    {{-- Imagen de la vacante --}}
+    <div>
+        <x-input-label for="imagen" :value="__('Imagen de la vacante')" />
+        <x-text-input
+            id="imagen"
+            class="block mt-1 w-full"
+            type="file"
+            name="imagen" />
+    </div>
+
+    {{-- Botón de confirmación --}}
+    <x-primary-button class="w-full justify-center">
+        {{ __('Crear vacante') }}
+    </x-primary-button>
+</form>
+
+{{--
+        <x-input-error :messages="$errors->get('email')" class="mt-2" /> --}}
