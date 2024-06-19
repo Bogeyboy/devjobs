891935b19210dd3858c53b768e62b1c3201e8843
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-06-19 20:38:32 +0200

    Clase 201

---
+++ database/migrations/2024_06_19_202141_add_columns_to_vacantes_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('vacantes', function (Blueprint $table) {
+            $table->string('titulo')->after('id');
+            $table->foreignId('salario_id')->constrained()->onDelete('cascade');
+            $table->foreignId('categoria_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('empresa');
+            $table->date('ultimo_dia');
+            $table->text('descripcion');
+            $table->string('imagen');
+            $table->integer('publicado')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('vacantes', function (Blueprint $table) {
+            /* $table->dropColumn('titulo');
+            $table->dropColumn('salario_id');
+            $table->dropColumn('categoria_id');
+            $table->dropColumn('user_id');
+            $table->dropColumn('empresa');
+            $table->dropColumn('ultimo_dia');
+            $table->dropColumn('descripcion');
+            $table->dropColumn('imagen');
+            $table->dropColumn('publicado'); */
+            $table->dropColumn([
+                'titulo',
+                'salario_id',
+                'categoria_id',
+                'user_id',
+                'empresa',
+                'ultimo_dia',
+                'descripcion',
+                'imagen',
+                'publicado']);
+        });
+    }
+};
