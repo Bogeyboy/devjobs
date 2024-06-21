32922a4ef3cbb19595c6f0c0e32752b2eae35fd5
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-06-21 12:41:46 +0200

    Clase 204

--- app/Livewire/CrearVacante.php
+++ app/Livewire/CrearVacante.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Categoria;
 use App\Models\Salario;
+use App\Models\Vacante;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 
@@ -35,12 +36,25 @@ class CrearVacante extends Component
 
         //Almacenar la imagen
         $imagen = $this->imagen->store('public/vacantes');
-        
-        $nombre_Imagen = str_replace('public/vacantes/','',$imagen);
 
-        dd($nombre_Imagen);
+        /* $nombre_imagen = str_replace('public/vacantes/','',$imagen); */
+        $datos['imagen'] = str_replace('public/vacantes/', '', $imagen);//De esta manera reescribimos la imagen
+
+        /* dd($nombre_Imagen); */
         
         //Crear la vacante
+
+        Vacante::create([
+            'titulo'=> $datos['titulo'],
+            'salario_id'=> $datos['salario'],
+            'categoria_id'=> $datos['categoria'],
+            'empresa'=> $datos['empresa'],
+            'ultimo_dia'=> $datos['ultimo_dia'],
+            'descripcion'=> $datos['descripcion'],
+            /* 'imagen' => $nombre_imagen, */
+            'imagen'=> $datos['imagen'],
+            'user_id'=> auth()->user()->id
+        ]);
 
         //Crear un mensaje
 
