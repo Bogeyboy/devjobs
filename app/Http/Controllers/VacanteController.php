f37b3a1d3e7bb023a2c41d79e8a6b0eef05723b3
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-06-24 13:11:06 +0200

    Clase 217

--- app/Http/Controllers/VacanteController.php
+++ app/Http/Controllers/VacanteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Vacante;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class VacanteController extends Controller
 {
@@ -44,9 +45,14 @@ class VacanteController extends Controller
      */
     public function edit(Vacante $vacante/* string $id */)
     {
-        return view('vacantes.edit',[
-            'vacante' => $vacante
-        ]);
+        //$this->authorize('update',$vacante);
+        if (Gate::allows('update', $vacante)) {
+            return view('vacantes.edit', [
+                'vacante' => $vacante
+            ]);
+        } else {
+            abort(403,'Acción no autorizada.');
+        }
     }
 
     /**
--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -3,12 +3,15 @@
         <div class="p-6 bg-white border-b border-gray-200 md:flex md:justify-between md:items-center dark:text-gray-500">
             {{-- Datos de la oferta publicada --}}
             <div class="space-y-3">
+                {{-- Titulo --}}
                 <a href="#" class="text-xl font-bold">
                     {{$vacante->titulo}}
                 </a>
+                {{-- Empresa --}}
                 <p class="text-sm text-gray-600 font-bold">
                     {{$vacante->empresa}}
                 </p>
+                {{-- Ultimo día de inscripción --}}
                 <p class="text-sm text-gray-500">
                     Último día de inscripción: {{$vacante->ultimo_dia->format('d/m/Y')}}
                 </p>
