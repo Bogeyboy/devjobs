024e4479aa049d34711b9dcc7ae6432c5df1ee26
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-07-01 17:31:16 +0200

    Clase 230

--- resources/views/livewire/postular-vacante.blade.php
+++ resources/views/livewire/postular-vacante.blade.php
@@ -1,3 +1,12 @@
-<div>
-    <p>Aquí se mostrará el formulario para inscribirse en la oferta</p>
+<div class="bg-gray-100 dark:bg-gray-500 p-5 mt-10 flex flex-col justify-center items-center">
+    <h3 class="text-center text-2xl font-bold my-4">
+        Inscribirme a esta vacante
+    </h3>
+    <form action="" class="w-96 mt-5">
+        @csrf
+        <div class="mb-4">
+            <x-input-label for="cv" :value="__('Currículum vitae (PDF)')"/>
+            <x-text-input for="cv" type="file" accept=".pdf" class="block mt-1 w-full"/>
+        </div>
+    </form>
 </div>
