d610d2c4362e4a7238fd92ccbbc1aa3b21adc116
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-07-04 21:09:55 +0200

    Clase 240

--- app/Http/Controllers/NotificacionController.php
+++ app/Http/Controllers/NotificacionController.php
@@ -11,6 +11,10 @@ class NotificacionController extends Controller
      */
     public function __invoke(Request $request)
     {
-        dd('Desde el controlador de notificaciones');
+        $notificaciones = auth()->user()->unreadNotifications;
+        
+        return view('notificaciones.index',[
+            'notificaciones' => $notificaciones
+        ]);
     }
 }
