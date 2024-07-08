61702d0e35c089c68ea2f299ad312c19a0cd80e9
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-07-08 08:39:44 +0200

    Clase 250

--- resources/views/livewire/home-vacantes.blade.php
+++ resources/views/livewire/home-vacantes.blade.php
@@ -1,4 +1,5 @@
 <div>
+    <livewire:filtrar-vacantes />
     <div class="py-12">
         <div class="max-w-7xl mx-auto">
             <h3 class="font-extra-bold text-4xl text-gray-800 dark:text-gray-200 mb-12">
